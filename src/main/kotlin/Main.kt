28a563c51b9187207b43b951bf8a16bf15cb65c6
Harun Sevinc <harun.sevinc@adesso.de>
2022-01-05 23:01:34 +0100

    Added operator functions

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -56,6 +56,20 @@ fun main() {
     val sophia = Person("Sophia")
     val claudia = Person("Claudia")
     sophia likes claudia
+
+    /*Operator Functions*/
+    fun main() {
+        //sampleStart
+        operator fun Int.times(str: String) = str.repeat(this)       // 1
+        println(2 * "Bye ")                                          // 2
+
+        operator fun String.get(range: IntRange) = substring(range)  // 3
+        val str = "Always forgive your enemies; nothing annoys them so much."
+        println(str[0..14])                                          // 4
+//sampleEnd
+    }
+
+
 }
 
 class Person(val name: String) {
